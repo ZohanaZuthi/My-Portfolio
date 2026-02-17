a1d710dddc1323d67a3cb519fb2f8848d47c4e46
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2026-02-17 16:35:56 +0600

    fixing navbar to sidebar

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Home, User, Briefcase, Mail, Layers, Star, Hammer,Menu} from "lucide-react";
 
 
